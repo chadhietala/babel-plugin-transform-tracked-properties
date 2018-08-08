92922fe4cebaef3a95245f53f30c91ce3668f46f
Steven Foote <sfoote@linkedin.com>
2018-08-08 14:51:23 -0600

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  plugins: [
+    'node'
+  ],
+  env: {
+    node: true
+  },
+  rules: {
+  },
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# babel-plugin-transform-tracked-properties
+
+Transform `import { component } from '@glimmer/component'` to `import { component } from '@ember/component'`
