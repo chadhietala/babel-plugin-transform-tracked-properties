401cb8ca7f4a17d4b2565aee92bfc7c1d9f03671
Steven Foote <sfoote@linkedin.com>
2018-08-08 17:02:44 -0600

    Handling multiple ImportSpecifiers

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const innerImportVisitor = {
-  StringLiteral(path) {
-    const packageName = path.node.value;
-    if (packageName === '@glimmer/component') {
-      path.node.value = '@ember/component';
-    }
-  }
-};
+const babel = require('babel-core');
+const { types } = babel;
 
 module.exports = function() {
   return {
     visitor: {
       ImportDeclaration(path) {
-        path.traverse(innerImportVisitor);
+        if (path.node.source.value === '@glimmer/component') {
+          let hasTrackedImport = false;
+          path.node.specifiers = path.node.specifiers.filter(specifier => {
+            if (specifier.type === 'ImportSpecifier' && specifier.imported.name === 'tracked') {
+              hasTrackedImport = true;
+              return false;
+            }
+            return true;
+          });
+
+          if (hasTrackedImport) {
+            path.insertAfter(
+              types.importDeclaration([types.importSpecifier(types.identifier('tracked'), types.identifier('tracked'))], types.stringLiteral('@ember/component'))
+            )
+          }
+
+          if (!path.node.specifiers.length) {
+            path.remove();
+          }
+        }
       }
     }
   }
--- tests/index.js
+++ tests/index.js
@@ -2,19 +2,38 @@ const QUnit = require('qunit');
 const plugin = require('../index');
 const babel = require('babel-core');
 
+const trimAndRemoveNewLines = str => {
+  str.split(/\n/).join('').trim();
+};
+
 const opts = {
   filename: 'test',
   plugins: [plugin]
 };
+
 QUnit.module('transform tracked');
 
 QUnit.test('transform import', assert => {
-  let transformed = babel.transform(`
+  const transformed = babel.transform(`
 import { tracked } from '@glimmer/component';
 import jquery from 'jquery';
 `, opts).code;
-  assert.equal(transformed.trim(), `
+  const expected = trimAndRemoveNewLines(`
 import { tracked } from '@ember/component';
 import jquery from 'jquery';
-`.trim());
+`);
+  assert.equal(trimAndRemoveNewLines(transformed), expected, 'Lonely tracked is converted');
+});
+
+QUnit.test('transform import, with multiple ImportSpecifiers', assert => {
+  const transformed = babel.transform(`
+import Component, { tracked, something } from '@glimmer/component';
+import jquery from 'jquery';
+`, opts).code;
+  const expected = `
+import Component, { something } from '@glimmer/component';
+import { tracked } from '@ember/component';
+import jquery from 'jquery';
+  `;
+  assert.equal(trimAndRemoveNewLines(transformed), trimAndRemoveNewLines(expected));
 })
